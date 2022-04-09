fe8671e5da4ef966c367bbce10d9320b1dc8381b
Timothy Moon <timhgmoon@gmail.com>
2022-04-08 21:24:37 -0700

    added solution, solution taken from leetcode discussion. Using as reference to come back to

--- leetcode/daily_temp.py
+++ leetcode/daily_temp.py
@@ -1,16 +1,33 @@
-# 739
 
+# 739
 #Brute force O(n^2) [not good]
+# def dailyTemperatures(temperatures):
+#   answer = []
+#   for i in range(len(temperatures)):
+#     current = temperatures[i]
+#     answer.append(0)
+#     for j in range(i + 1, len(temperatures)):
+#       if current < temperatures[j]:
+#         answer[i] = j - i
+#         break
+#   return answer
+
+#taken off discussion page **NOT MY WORK** (using as reference)
+#link to original work
+#https://leetcode.com/problems/daily-temperatures/discuss/1574808/C%2B%2BPython-3-Simple-Solutions-w-Explanation-Examples-and-Images-or-2-Monotonic-Stack-Approaches
 def dailyTemperatures(temperatures):
-  answer = []
-  for i in range(len(temperatures)):
-    current = temperatures[i]
-    answer.append(0)
-    for j in range(i + 1, len(temperatures)):
-      if current < temperatures[j]:
-        answer[i] = j - i
-        break
-  return answer
+  ans = [0] * len(temperatures)
+
+  #stack
+  s = []
+
+  for current in range(len(temperatures)-1, -1, -1):
+    while s and temperatures[s[-1]] <= temperatures[current]:
+      s.pop()
+    ans[current] = s[-1] - current if s else 0
+    s.append(current)
+  return ans
+
 
 temperatures = [73,74,75,71,69,72,76,73]
 print(dailyTemperatures(temperatures))
