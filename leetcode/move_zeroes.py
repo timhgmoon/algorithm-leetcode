7b37d6682bc87f7f1c59fbb41a7a3da8c1efe004
Tim Moon <timhgmoon@gmail.com>
2022-06-23 00:35:40 -0700

    count amount of zeroes, remove amount of zeroes, append zeroes to end

---
+++ leetcode/move_zeroes.py
@@ -0,0 +1,18 @@
+# 283. Move Zeroes
+
+def moveZeroes(nums):
+  """
+  Do not return anything, modify nums in-place instead.
+  """
+  amount_to_remove = 0
+  for num in nums:
+    if num == 0:
+      amount_to_remove += 1
+  
+  for i in range(amount_to_remove):
+    nums.remove(0)
+    nums.append(0)
+  
+nums = [0,1,0,3,12]
+moveZeroes(nums)
+print(nums)
